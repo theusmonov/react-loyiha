9d186859e1f045002caaf6cfc3ab923a18d195ef
theusmonov <asadbek.usmonoff2003@gmail.com>
2023-04-02 16:50:58 +0500

    react loyiha

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Container from './components/Container/Container'
+import Nav from './components/header/Nav'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='container'>
+       
+    </div>
+  )
+}
+
+export default App
